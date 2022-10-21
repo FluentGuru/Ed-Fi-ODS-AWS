75de71d3d8d33bcbb1570483b7a59f9452163776
FluentGuru <paulo.enmanuel@hotmail.com>
2022-10-21 17:52:33 -0400

    Adding missing documentation

--- README.md
+++ README.md
@@ -5,3 +5,77 @@
 | Databse engine | PostgreSQL |
 | Database engine version | 13.X |
 | Admin app version | 2.3.1 |
+
+# GUIDE
+
+## 1.VPC
+
+1.1. Create an AWS Virtual Private network with:
+- 2 Availability zones (AZ)
+- 2 public subnets (One per AZ)
+- 2 private subnets (One per AZ)
+- 2 NAT Gateways (One per AZ)
+- 1 internet gateway
+- 1 Elastic IP
+- 1 Route table
+- 1 Network access controller
+- 1 default security group
+
+1.2. Configure security groups
+- Add a rule for the PostgreSql protocol allowing your IP
+- Add a rule for HTTP/HTTPS allowing any connecion
+
+1.3. Copy VPC ID
+1.4. Copy Security group ID
+
+## 2. RDS
+2.1. Create an RDS instance with:
+- PostgreSql Engine
+- PostgreSql version 13.X
+- Free tier of multi-AZ
+- a default DB subnet with the created VPC and default security group
+- Publicly accessible (Will change later)
+- IAM and PostgreSQL authentication
+
+2.2. Copy RDS Hostname, port, username and password
+
+## 3. Elastic load balancer
+3.1. Create an ELB Target group with:
+- Empty listing
+- No listeners
+3.2. Create an Application load balancer with
+- the empty target group assigned
+- Associated to the created VPC
+- With the VPC default Security group
+
+# 4. ECS & ECR
+4.1. Create an ECR repository with:
+- Public access
+
+4.2. copy the repository publisher URL
+
+4.3. Create empty ECS cluster with
+- A sample name
+- Fargate provisioning
+- Assigned to the created VPC
+
+4.5. Copy ECS ID and name
+
+# 5. Install local dependencies
+5.1. Install direnv
+5.2. Install plsql
+5.3. Install docker
+5.4. Install openssl 1.1
+
+# 6 Setup environment
+6.1. Configure environment variables on file `.envrc` (See comments in file)
+6.2. run `direnv allow` to apply environment configuration
+
+# 7 Run scripts
+7.1. Publish images by running `exec publish.sh`
+7.2. deploy container running `exec deploy.sh`
+
+# 8 Test deployment
+8.1. Open API at `{LOAD_BALANCER_URL}:80`
+8.2. Open Swagger at `{LOAD_BALANCER_URL}:8081`
+8.3. Open Adminapp at `{LOAD_BALANCER_URL}:8082`
--- publish.sh
+++ publish.sh
@@ -1,27 +1,22 @@
-#AWS CREDENTIALS
-export AWS_ACCESS_KEY_ID="ASIAQZ5DTLYWENN3KEBI"
-export AWS_SECRET_ACCESS_KEY="4BlW/R/cPjTPaW0GWoG2NKUfeTovnKOQHFmA8CWS"
-export AWS_SESSION_TOKEN="IQoJb3JpZ2luX2VjEI///////////wEaCXVzLWVhc3QtMSJHMEUCIQD6aiNCgN5cl24M1xvPsP+tfFtxdXxuP1j2TPur96BOewIgPTp666/yE3pa0Cvwf8/YMIY6TV2G68cnIXhn9PBnVxsq9wIIaBABGgwwNTU2NDA4MDkwMDQiDNksNpxcsF/ET029tyrUAqIFuojGMe5Bte0fxhAbae1dmfD2p3MpzpTbEY36eqlzuKRX7qoqg95+T6H+8QqMnNAuN1g6lP7McseMga2aKwsdE/HY39TXcZyJA+z3Asi8IdcY39ohTotAJ509hduCIdS/mTnQiBnvm7tNO3qxKXj9PEz0r2gtkBA5Y+hO51BIV3ww7W8q42wdTwOpiJ23naxAdCf26nd0H9T0SFm3/CRj509JfAF13Gl8R1mH0stfrGtwG1ONCOz3JoaxOLhIsrnEipfm1UaxS7h8pcPbPqQTMmkqCO7K62AZ0SQJHBIe8tBPz9puuyHh/djYFSugI258hn2yQqGUy/if07p8YIZF09D8AWMtY94cRYPapWDt8j+Lx/QCupyq45xlN3NnpqdXsATpVqWlf9hc52gKjhG2GHknNPqFcB2JDqEvO2qH/PE/vyzGkoAPzWb1IZwLs56k8pwwqpnHmgY6pwHTSjQBHHhC2tXyaUbI4m1Sw6SMhk/hi3PyP0Xht24uC+XIfy3IEa0c4jQCuyPs3AUhKukr0D+kLIZN56jBMV//4otsW5pHRqKhc3lTlI5A/Zy+M/i7ipdKLNSWn0RfPJx6iEKT4rmSAG/a9KXezVQAKMgZ6j6uWh2YN4DgI/AQFDJzlTHDyP6w5zbGunsdkCRYszhGazBK/HPPz15MMcQjnQsEIoUm9w=="
-
 #SETUP
 echo "Setting up "
 docker context use default
-# aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/s2q7l7m0
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ECR_URL
 
 #ODS
 echo "Publishing ODS API"
-docker build -t public.ecr.aws/s2q7l7m0/developersnet-edfi-api:latest ./images/ods/ 
-docker push public.ecr.aws/s2q7l7m0/developersnet-edfi-api:latest
+docker build -t $AWS_ECR_URL/developersnet-edfi-api:latest ./images/ods/ 
+docker push $AWS_ECR_URL/developersnet-edfi-api:latest
 
 #ADMIN
 echo "Publishing ADMINAPP"
-docker build -t public.ecr.aws/s2q7l7m0/developersnet-edfi-adminapp:latest ./images/adminapp/
-docker push public.ecr.aws/s2q7l7m0/developersnet-edfi-adminapp:latest
+docker build -t $AWS_ECR_URL/developersnet-edfi-adminapp:latest ./images/adminapp/
+docker push $AWS_ECR_URLdevelopersnet-edfi-adminapp:latest
 
 #SWAGGER
 echo "Publishing SWAGGER"
-docker build -t public.ecr.aws/s2q7l7m0/developersnet-edfi-swagger:latest ./images/swagger/
-docker push public.ecr.aws/s2q7l7m0/developersnet-edfi-swagger:latest
+docker build -t $AWS_ECR_URL/developersnet-edfi-swagger:latest ./images/swagger/
+docker push $AWS_ECR_URL/developersnet-edfi-swagger:latest
 
 
 
